a91684c6699297c123e83551732da9c3669827e5
realAfoke <ighoafokereality3@gmail.com>
2025-08-22 22:26:02 +0100

    Edited procfile file

--- backend/setting/settings.py
+++ backend/setting/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'setting.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=config("DATABASE_URL")
     )
 }
 
